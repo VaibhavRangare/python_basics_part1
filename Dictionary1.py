cf250f4d7b59d5711301262888e86c3f5a96085f
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2019-09-18 12:27:35 +0530

    Create Dictionary1.py

---
+++ Dictionary1.py
@@ -0,0 +1,8 @@
+dic1 = {"age": 20, "Name": "John", "Dept": "Chemistry"}
+print(dic1)
+# print(dict[0])                # will not work
+print(dic1.get("age"))
+list1 = dic1.keys()
+list2 = dic1.values()
+print(list1)
+print(list2)
