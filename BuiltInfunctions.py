3f08c941a774c5d605633d8d38fd7b998305ba97
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2019-09-18 12:25:43 +0530

    Create BuiltInfunctions.py

---
+++ BuiltInfunctions.py
@@ -0,0 +1,9 @@
+# name = input("Hello:")
+# print("Hello:v"+ name)
+print(abs(-10))
+# print(any(10))
+print(ascii(10))
+print(len("abcdef"))
+list1 = [1, 2, 3, 4]
+print(sum(list1))
+print(sorted(list1))
