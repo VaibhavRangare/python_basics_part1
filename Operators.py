1a5cabdb0f16e38e8b2530244ecdce92fa33a7fe
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2019-09-18 12:33:46 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,28 @@
+if True and False:                  # False
+    print('If condition')
+else:
+    print('Else Condition')
+if True and not False:                  # False, & not will not work, and not will work
+    print('If condition')
+else:
+    print('Else Condition')
+if True & False:                  # False
+    print('If condition')
+else:
+    print('Else Condition')
+if True or False:                  # True
+    print('If condition')
+else:
+    print('Else Condition')
+if True | False:                  # True
+    print('If condition')
+else:
+    print('Else Condition')
+if True ^ False:                  # True
+    print('If condition')
+else:
+    print('Else Condition')
+if True ^ False:                  # True
+    print('If condition')
+else:
+    print('Else Condition')
