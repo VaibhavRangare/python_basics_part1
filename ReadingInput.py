075bc9a7f3645fea129eae99de18a871411126af
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2019-09-18 12:36:43 +0530

    Create ReadingInput.py

---
+++ ReadingInput.py
@@ -0,0 +1,6 @@
+# Every input value is treated as String and need to convert accordingly
+name = input('Hi!! What is your name:')
+print('Hello !!! ' + name)
+birth_year = input('Birth Year:')
+age = 2019 - int(birth_year)
+print(age)
