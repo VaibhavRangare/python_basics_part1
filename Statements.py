0564038a0d0f19b166bd5adee9455a9ccd13bf0b
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2019-09-18 12:37:31 +0530

    Create Statements.py

---
+++ Statements.py
@@ -0,0 +1,18 @@
+test = 5
+if test == 5:
+    print("test is 5")
+if test == 5:
+    print("test is 5")
+if test < 4:
+    print("test is less than 4")
+else:
+    print("if condition fails")
+
+if test < 4:
+    print("test is less than 4")
+elif test > 5:
+    print("test is > than 4")
+else:
+    print("test is 5")
+    print(f'test is {test}')            # use of formatted string in print
+    print('test is {test}')             # without f'' output is test is {test}
