3d89cfb850c1caf937405767397184c9d2e9e0e3
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2019-09-18 12:24:26 +0530

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,15 @@
+print(1+1)              # 2
+print(1++1)             # 2
+print(1-1)              # 0
+print(1--1)             # 2
+print(1 / 2)            # .5
+print(1//2)             # 0
+print(5 % 2)              # 1
+print(2**3)             # 8
+x = 10
+x += 3
+print(x)                # 13
+x = 10 + 3 * 2
+print(x)                # 16
+x = (10+3) * 2
+print(x)                # 26
