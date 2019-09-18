ce9627c85dbc79ecef17510ff069352bf7c96f32
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2019-09-18 12:38:25 +0530

    Create StringMethods.py

---
+++ StringMethods.py
@@ -0,0 +1,11 @@
+course = 'Python'
+print(course.upper())                   # PYTHON
+print(course.lower())                   # pythn
+print(course.capitalize())
+print(course.casefold())                #
+print(course.replace("P", "J"))          # Jython
+print(course)
+# check if string or char in String
+print(course.find('y'))                 # 1 index of the char
+print("P" in course)                    # True
+print("p" in course)                    # False
